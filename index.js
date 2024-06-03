35e7706f7668f0e1ba5cb831c93589e064fbf9fe
Avishek Chakraborty <avishek3141@gmail.com>
2024-06-03 21:20:43 +0530

    Initial commit Kpis done ✔️

---
+++ index.js
@@ -0,0 +1,39 @@
+import express from "express";
+import mongoose from "mongoose";
+import cors from "cors";
+import dotenv from "dotenv";
+import helmet from "helmet";
+import morgan from "morgan";
+import kpiRoutes from "./routes/kpi.js";
+import KPI from "./models/KPI.js";
+import { kpis } from "./data/data.js";
+
+// Configuration -->
+dotenv.config();
+const app = express();
+app.use(express.json()); // Parse JSON bodies (as sent by API clients)
+app.use(express.urlencoded({ extended: false })); // Parse URL-encoded bodies (as sent by HTML forms)
+app.use(helmet()); // Secure your Express apps by setting various HTTP headers
+app.use(helmet.crossOriginResourcePolicy({ policy: "cross-origin" })); // Set Cross-Origin Resource Policy to "cross-origin"
+app.use(morgan("common")); // HTTP request logger middleware
+app.use(cors()); // Enable All CORS Requests
+ 
+// Routes -->
+app.use("/kpi", kpiRoutes);
+
+app.use("/products", productRoutes);
+
+// MongoDB
+const PORT = process.env.PORT || 9000;
+mongoose
+	.connect(process.env.MONGO_URL, {})
+	.then(async () => {
+		app.listen(PORT, () => console.log(`Server Port: ${PORT}`));
+
+		// Seed our data base only when needed -->
+		// await mongoose.connection.db.dropDatabase();
+		// KPI.insertMany(kpis);
+	})
+	.catch((error) => console.log(`Did not connect ❌ \n ${error}`));
+
+console.log("Hello");
---
+++ models/KPI.js
@@ -0,0 +1,91 @@
+import mongoose from "mongoose";
+import { loadType } from "mongoose-currency";
+
+const Schema = mongoose.Schema;
+loadType(mongoose);
+
+const monthSchema = new Schema(
+	{
+		month: String,
+		revenue: {
+			type: mongoose.Types.Currency,
+			currency: "INR",
+			get: (v) => v / 100,
+		},
+		expenses: {
+			type: mongoose.Types.Currency,
+			currency: "INR",
+			get: (v) => v / 100,
+		},
+		operationalExpenses: {
+			type: mongoose.Types.Currency,
+			currency: "INR",
+			get: (v) => v / 100,
+		},
+		nonOperationalExpenses: {
+			type: mongoose.Types.Currency,
+			currency: "INR",
+			get: (v) => v / 100,
+		},
+	},
+	{ toJSON: { getters: true } }
+);
+
+/**
+ * A helper function that converts a currency value to a value in rupees. It
+ * divides the value by 100 because mongoose stores currency values as a
+ * float in the range [100, 1000000000000] in units of 100 paisa.
+ */
+const daySchema = new Schema(
+	{
+		date: String,
+		revenue: {
+			type: mongoose.Types.Currency,
+			currency: "INR",
+			get: (v) => v / 100,
+		},
+		expenses: {
+			type: mongoose.Types.Currency,
+			currency: "INR",
+			get: (v) => v / 100,
+		},
+	},
+	{ toJSON: { getters: true } }
+);
+
+const KPISchema = new Schema(
+	{
+		totalProfit: {
+			type: mongoose.Types.Currency,
+			currency: "INR",
+			get: (v) => v / 100,
+		},
+		totalRevenue: {
+			type: mongoose.Types.Currency,
+			currency: "INR",
+			get: (v) => v / 100,
+		},
+		totalExpenses: {
+			type: mongoose.Types.Currency,
+			currency: "INR",
+			get: (v) => v / 100,
+		},
+		expenseByCategory: {
+			type: Map,
+			of: {
+				type: mongoose.Types.Currency,
+				currency: "INR",
+				get: (v) => v / 100,
+			},
+		},
+		monthlyData: [monthSchema],
+
+		dailyData: [daySchema],
+	},
+	{ toJSON: { getters: true }, timestamps: true }
+);
+
+
+const KPI = mongoose.model("KPI", KPISchema);
+
+export default KPI;
