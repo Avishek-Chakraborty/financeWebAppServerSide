ff3ed3662e3df3a564c1448297d96e153ce7fd94
Avishek Chakraborty <avishek3141@gmail.com>
2024-06-04 11:05:04 +0530

    Transaction route done partially

--- index.js
+++ index.js
@@ -4,11 +4,17 @@ import cors from "cors";
 import dotenv from "dotenv";
 import helmet from "helmet";
 import morgan from "morgan";
-import kpiRoutes from "./routes/kpi.js";
+
+// For DB seeding -->
+import { kpis, products, transactions } from "./data/data.js";
 import KPI from "./models/KPI.js";
-import { kpis, products } from "./data/data.js";
-import productRoutes from "./routes/product.js";
 import Product from "./models/Product.js"
+import Transaction from "./models/Transaction.js"
+
+import kpiRoutes from "./routes/kpi.js";
+import productRoutes from "./routes/product.js";
+import transactionRoutes from "./routes/transaction.js"
+
 
 // Configuration -->
 dotenv.config();
@@ -22,8 +28,8 @@ app.use(cors()); // Enable All CORS Requests
  
 // Routes -->
 app.use("/kpi", kpiRoutes);
-
 app.use("/product", productRoutes);
+app.use("/transaction", transactionRoutes)
 
 // MongoDB
 const PORT = process.env.PORT || 9000;
@@ -37,6 +43,7 @@ mongoose
 		// KPI.insertMany(kpis);
 		// KPI.insertMany(kpis);
 		// Product.insertMany(products);
+		// Transaction.insertMany(transactions);
 	})
 	.catch((error) => console.log(`Did not connect ❌ \n ${error}`));
 
